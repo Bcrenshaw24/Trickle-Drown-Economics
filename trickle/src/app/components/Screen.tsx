268d62ebe9d62264435833dc477ef3fd2bd673c2
Branden <bcrensh6@charlotte.edu>
2025-07-09 18:55:08 -0400

    Added Game Tabs
    Added functionality to the tabs, making them more dynamic.

--- trickle/src/app/components/Screen.tsx
+++ trickle/src/app/components/Screen.tsx
@@ -3,17 +3,23 @@ import React from 'react'
 import { useState } from 'react'
 import localFont from 'next/font/local'
 import { RxHamburgerMenu } from "react-icons/rx";
+import News from './News';
+import Budget from './Budget';
+import Charts from './Charts'
+import Events from './Events' 
+import Team from './Team'
 
 
 const myFont = localFont({ src: '../fonts/Pop.ttf', weight: "400" })
 
 const Screen = () => {
-    const[activeView, setActiveView] = useState< "News" | "Budget" | "Charts" | "Events" | "Team" >("News")
     const[month, setMonth] = useState(1)
     const[money, setMoney] = useState(20000)
 
-    const tabs: string[] = ["News", "Budget", "Charts", "Events", "Team"]
-    const[isOpen, setIsOpen] = useState(false)
+    const tabs = {"News": <News/>, "Budget": <Budget/>, "Charts": <Charts/>, "Events": <Events/>, "Team": <Team/>}
+    const[isOpen, setIsOpen] = useState(false) 
+    const[activeView, setActiveView] = useState<keyof typeof tabs>("News")
+
 
   return (
     <div className={`flex flex-col justify-start w-3/4 h-4/5 bg-sky-950 border-4 border-sky-700 rounded-2xl ${myFont.className} `}> 
@@ -27,9 +33,10 @@ const Screen = () => {
             <div className='text-4xl'>${money}</div>
         </div> 
         { isOpen &&
-        <div className="flex flex-col justify-start bg-sky-950 border-r-4 border-sky-700 text-2xl w-1/3 h-full rounded">
-            {tabs.map((item) => (
-              <button key={item} className={`${item == activeView ? "text-red-600 " : "text-sky-600 hover:text-sky-800"} flex justify-start ml-3 hover:cursor-pointer`}>{item}</button> 
+        <div className="flex flex-col justify-start bg-sky-950 border-r-4 border-sky-600 text-2xl w-1/3 h-full rounded">
+            {Object.keys(tabs).map(item => (
+
+              <button key={item} onClick={() => setActiveView(item as keyof typeof tabs)} className={`${item === activeView ? "text-red-600 " : "text-sky-400 hover:text-sky-600"} flex justify-start ml-3 hover:cursor-pointer border-b-4 border-sky-600`}>{item}</button> 
             )
         ) 
     }
@@ -37,6 +44,9 @@ const Screen = () => {
 
         </div> 
         }
+        <div> 
+        {tabs[activeView]}
+        </div>
     </div>
   )
 }
